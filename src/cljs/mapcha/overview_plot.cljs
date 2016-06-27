d7bb362be810dd58744aed91d59928535c67d484
Gary Johnson <gwjohnso@uvm.edu>
2016-06-27 15:56:13 -0400

    Rename lookup-report-addresses to plot-list in mapcha.overview-plot.

--- src/cljs/mapcha/overview_plot.cljs
+++ src/cljs/mapcha/overview_plot.cljs
@@ -2,7 +2,7 @@
   (:require [goog.dom :as dom]
             [mapcha.map-utils :as map]))
 
-(defn lookup-report-addresses []
+(def plot-list
   [["Cambodia"     "104.916667" "11.55"     "10.0"]
    ["Thailand"     "100.483333" "13.75"     "10.0"]
    ["Vietnam"      "105.85"     "21.033333" "10.0"]
@@ -10,5 +10,5 @@
    ["Myanmar"      "96.1"       "19.75"     "10.0"]])
 
 (defn ^:export main []
-  (map/init-overview-map "overview-map" (lookup-report-addresses))
+  (map/init-overview-map "overview-map" plot-list)
   (dom/removeNode (dom/getElement "overview-map-wait-message")))
