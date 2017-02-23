0750c3012b286a80a7af18f05155dddfd473db46
Gary Johnson <gwjohnso@uvm.edu>
2017-02-22 20:31:32 -0500

    Completed the translation of mapcha.map_utils from CLJS to JS.

--- src/cljs/mapcha/map_utils.js
+++ src/cljs/mapcha/map_utils.js
@@ -43,6 +43,10 @@ map_utils.get_full_extent = function () {
 // This reference will hold the current page's OpenLayers map object
 map_utils.map_ref = null;
 
+// Example call:
+// map_utils.digital_globe_base_map({"div_name":      "image-analysis-pane",
+//                                   "center_coords": [102.0, 17.0],
+//                                   "zoom_level":    5});
 map_utils.digital_globe_base_map = function (map_config) {
     var digital_globe_access_token     = "pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYS" +
                                          "I6ImNpcTJ3ZTlyZTAwOWNuam00ZWU3aTk" +
@@ -122,6 +126,12 @@ map_utils.digital_globe_base_map = function (map_config) {
     return openlayers_map;
 };
 
+/*****************************************************************************
+***
+*** Functions to switch the visible basemap imagery and zoom to a layer
+***
+*****************************************************************************/
+
 map_utils.current_imagery = "DigitalGlobeRecentImagery+Streets";
 
 map_utils.set_current_imagery = function (new_imagery) {
@@ -144,6 +154,12 @@ map_utils.zoom_map_to_layer = function (layer) {
     var extent = layer.getSource().getExtent();
     return view.fit(extent, size);
 };
+
+/*****************************************************************************
+***
+*** Functions to draw project boundaries and plot buffers
+***
+*****************************************************************************/
 
 map_utils.styles =
     {"icon": new ol.style.Style(
@@ -212,4 +228,187 @@ map_utils.draw_buffer = function (center, radius) {
     map_utils.map_ref.addLayer(vector_layer);
     map_utils.zoom_map_to_layer(vector_layer);
     return map_utils.map_ref;
+};
+
+/*****************************************************************************
+***
+*** Functions to setup select interactions for click and click-and-drag events
+***
+*****************************************************************************/
+
+map_utils.feature_styles = {};
+
+map_utils.select_interaction = null;
+
+map_utils.make_click_select = function (layer) {
+    var select = new ol.interaction.Select({"layers": [layer]});
+    var action = function (event) {
+        event.selected.forEach(function (feature) {
+            map_utils.feature_styles[feature] = feature.getStyle();
+            feature.setStyle(null);
+        });
+        event.deselected.forEach(function (feature) {
+            var saved_style = map_utils.feature_styles[feature];
+            if (saved_style != null) {
+                feature.setStyle(saved_style);
+            }
+        });
+    };
+    select.on("select", action);
+    return select;
+};
+
+map_utils.dragbox_interaction = null;
+
+map_utils.make_dragbox_select = function (layer, selected_features) {
+    var condition = ol.events.condition.platformModifierKeyOnly;
+    var dragbox = new ol.interaction.DragBox({"condition": condition});
+    var source = layer.getSource();
+    var boxstart_action = function (event) {
+        selected_features.clear();
+    };
+    var boxend_action = function (event) {
+        var extent = dragbox.getGeometry().getExtent();
+        var save_style = function (feature) {
+            selected_features.push(feature);
+            map_utils.feature_styles[feature] = feature.getStyle();
+            feature.setStyle(null);
+            return false;
+        };
+        source.forEachFeatureIntersectingExtent(extent, save_style);
+    };
+    dragbox.on("boxstart", boxstart_action);
+    dragbox.on("boxend", boxend_action);
+    return dragbox;
+};
+
+map_utils.enable_selection = function (layer) {
+    var click_select = map_utils.make_click_select(layer);
+    var selected_features = click_select.getFeatures();
+    var dragbox_select = map_utils.make_dragbox_select(layer, selected_features);
+    map_utils.map_ref.addInteraction(click_select);
+    map_utils.map_ref.addInteraction(dragbox_select);
+    map_utils.select_interaction = click_select;
+    map_utils.dragbox_interaction = dragbox_select;
+    return null;
+};
+
+map_utils.disable_selection = function () {
+    if (map_utils.select_interaction != null) {
+        map_utils.map_ref.removeInteraction(map_utils.select_interaction);
+        map_utils.select_interaction = null;
+    }
+    if (map_utils.dragbox_interaction != null) {
+        map_utils.map_ref.removeInteraction(map_utils.dragbox_interaction);
+        map_utils.dragbox_interaction = null;
+    }
+    return null;
+};
+
+/*****************************************************************************
+***
+*** Functions to draw sample points inside a plot
+***
+*****************************************************************************/
+
+map_utils.current_samples = null;
+
+map_utils.remove_sample_layer = function () {
+    if (map_utils.current_samples != null) {
+        map_utils.map_ref.removeLayer(map_utils.current_samples);
+        map_utils.current_samples = null;
+    }
+    return null;
+};
+
+map_utils.draw_points = function (samples) {
+    var features = [];
+    for (sample in samples) {
+        var format = new ol.format.GeoJSON();
+        var latlon = format.readGeometry(sample["point"]);
+        var geometry = latlon.transform("EPSG:4326", "EPSG:3857");
+        var feature = new ol.Feature({"geometry": geometry,
+                                      "sample_id": sample["id"]});
+        features.push(feature);
+    }
+    var vector_source = new ol.source.Vector({"features": features});
+    var style = map_utils.styles["red_point"];
+    var vector_layer = new ol.layer.Vector({"source": vector_source,
+                                            "style": style});
+    map_utils.remove_sample_layer();
+    map_utils.current_samples = vector_layer;
+    map_utils.disable_selection();
+    map_utils.map_ref.addLayer(vector_layer);
+    map_utils.enable_selection(vector_layer);
+    return map_utils.map_ref;
+};
+
+map_utils.get_selected_samples = function () {
+    if (map_utils.select_interaction != null) {
+        return map_utils.select_interaction.getFeatures();
+    } else {
+        return null;
+    }
+};
+
+map_utils.highlight_sample = function (sample, color) {
+    var fill = new ol.style.Fill({"color": color || "#999999"});
+    var stroke = new ol.style.Stroke({"color": "#000000",
+                                      "width": 2});
+    var circle = new ol.style.Circle({"radius": 5,
+                                      "fill": fill,
+                                      "stroke": stroke});
+    var style = new ol.style.Style({"image": circle});                                   sample.setStyle(style);
+    return null;
+};
+
+/*****************************************************************************
+***
+*** Bounding Box Selector for Admin Page
+***
+*****************************************************************************/
+
+map_utils.dragbox_draw_layer = null;
+
+map_utils.dragbox_draw_interaction = null;
+
+map_utils.current_bbox = null;
+
+map_utils.enable_dragbox_draw = function () {
+    var source = new ol.source.Vector({"features": []});
+    var style = map_utils.styles["polygon"];
+    var draw_layer = new ol.layer.Vector({"title": "ProjectBoundingBox",
+                                          "source": source,
+                                          "style": style});
+    var condition = ol.events.condition.platformModifierKeyOnly;
+    var dragbox = new ol.interaction.DragBox({"condition": condition});
+    var boxend_action = function (event) {
+        var geom = dragbox.getGeometry();
+        var feature = new ol.Feature({"geometry": geom});
+        var extent = geom.clone().transform("EPSG:3857", "EPSG:4326").getExtent();
+        source.clear();
+        source.addFeature(feature);
+        map_utils.current_bbox = {"minlon": extent[0],
+                                  "minlat": extent[1],
+                                  "maxlon": extent[2],
+                                  "maxlat": extent[3]};
+    };
+    dragbox.on("boxend", boxend_action);
+    map_utils.map_ref.addLayer(draw_layer);
+    map_utils.map_ref.addInteraction(dragbox);
+    map_utils.dragbox_draw_layer = draw_layer;
+    map_utils.dragbox_draw_interaction = dragbox;
+    return null;
+};
+
+map_utils.disable_dragbox_draw = function () {
+    if (map_utils.dragbox_draw_layer != null) {
+        map_utils.map_ref.removeLayer(map_utils.dragbox_draw_layer);
+        map_utils.dragbox_draw_layer = null;
+    }
+    if (map_utils.dragbox_draw_interaction != null) {
+        map_utils.map_ref.removeInteraction(map_utils.dragbox_draw_interaction);
+        map_utils.dragbox_draw_interaction = null;
+    }
+    return null;
 };
